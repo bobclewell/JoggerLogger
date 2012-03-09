90c29d9cd27c20517af2b38e81a730b303eefc48
Bob Clewell <bobclewell@gmail.com>
2012-03-08 21:56:54 -0500

    Updated jogged_at acceptor and input/output of time

--- app/models/jog.rb
+++ app/models/jog.rb
@@ -28,9 +28,27 @@ class Jog < ActiveRecord::Base
     elsif time_string.length == 1
       time_in_seconds = time_string[0].to_i
     end
+    self.seconds = time_in_seconds
   end
 
   def seconds_to_time(seconds)
+    hours = seconds / 3600
+    minutes = (seconds / 60) - (hours * 60)
+    actual_seconds = seconds%60
+    if hours > 0
+      "#{hours}:#{minutes}:#{actual_seconds}"
+    else
+      "#{minutes}:#{actual_seconds}"
+    end
+  end
+
+  def display_time
+    seconds_to_time(self.seconds) unless self.seconds.nil?
+  end
+
+  def display_time=(time_string)
+    self.seconds = time_to_seconds(time_string)
+    self.save
   end
 
   def jogged_at_string
@@ -38,7 +56,8 @@ class Jog < ActiveRecord::Base
   end
 
   def jogged_at_string=(jogged_at_str)
-    self.jogged_at = Date.strptime jogged_at_str, '%m/%d/%Y' unless self.jogged_at.nil?
+    self.jogged_at = Date.strptime jogged_at_str, '%m/%d/%Y' unless jogged_at_str.nil?
+    self.save
   end
 
   def logically_delete
