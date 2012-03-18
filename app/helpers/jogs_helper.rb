7bfb835e205eb1803d4d388a6c06143138186a92
Bob Clewell <bobclewell@gmail.com>
2012-03-17 22:40:31 -0400

    Adding humanized goal statement to home page. (in progress)

--- app/helpers/jogs_helper.rb
+++ app/helpers/jogs_helper.rb
@@ -1,2 +1,18 @@
 module JogsHelper
+  def goal_statement(current_user)
+    case current_user.current_goal.goal_type
+    when "1"
+      "Run #{current_user.current_goal.distance} #{current_user.distance_unit}."
+    when "2"
+      "Run for #{current_user.current_goal.seconds_to_time_humanized(current_user.current_goal.time)}."
+    when "3"
+      true if current_user.current_goal.single_distance_in_time_achieved?(self, current_user)
+    when "4"
+      true if current_user.current_goal.single_distance_at_pace_achieved?(self, current_user)
+    when "5"
+      true if current_user.current_goal.multi_distance_achieved?(current_user)
+    when "6"
+      true if current_user.current_goal.multi_distance_in_time_achieved?(current_user)
+    end
+  end
 end
