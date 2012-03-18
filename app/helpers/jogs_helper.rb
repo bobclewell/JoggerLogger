e0b8b69cd0722c5e0460241a4c8db730f03b13cf
Bob Clewell <bobclewell@gmail.com>
2012-03-18 00:38:35 -0400

    Better language for helper.

--- app/helpers/jogs_helper.rb
+++ app/helpers/jogs_helper.rb
@@ -1,18 +1,22 @@
 module JogsHelper
   def goal_statement(current_user)
+    if current_user.current_goal.present?
     case current_user.current_goal.goal_type
-    when "1"
-      "Run #{current_user.current_goal.distance} #{current_user.distance_unit}."
-    when "2"
-      "Run for #{current_user.current_goal.seconds_to_time_humanized(current_user.current_goal.time)}."
-    when "3"
-      "Run #{current_user.current_goal.distance} #{current_user.distance_unit} in #{current_user.current_goal.seconds_to_time_humanized(current_user.current_goal.time)}."
-    when "4"
-      "Run at a pace of #{current_user.current_goal.seconds_to_time_humanized(current_user.current_goal.time)} for #{current_user.current_goal.distance} #{current_user.distance_unit}."
-    when "5"
-      "Run #{current_user.current_goal.distance.to_i} #{current_user.distance_unit}."
-    when "6"
-      "Run #{current_user.current_goal.distance.to_i} #{current_user.distance_unit} in #{current_user.current_goal.time} #{current_user.current_goal.time_unit}."
+      when "1"
+        "Run #{current_user.current_goal.distance} #{current_user.distance_unit}."
+      when "2"
+        "Run for #{current_user.current_goal.seconds_to_time_humanized(current_user.current_goal.time)}."
+      when "3"
+        "Run #{current_user.current_goal.distance} #{current_user.distance_unit} in #{current_user.current_goal.seconds_to_time_humanized(current_user.current_goal.time)}."
+      when "4"
+        "Run at a pace of #{current_user.current_goal.seconds_to_time_humanized(current_user.current_goal.time)} for #{current_user.current_goal.distance} #{current_user.distance_unit}."
+      when "5"
+        "Run #{current_user.current_goal.distance.to_i} #{current_user.distance_unit}."
+      when "6"
+        "Run #{current_user.current_goal.distance.to_i} #{current_user.distance_unit} in #{current_user.current_goal.time} #{current_user.current_goal.time_unit}."
+      end
+    else
+      "You need a new goal - need to make this a link!"
     end
   end
 end
