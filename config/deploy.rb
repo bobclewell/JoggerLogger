152f52d899f18ec72ffccafb4220be0561b1a9ba
Bob Clewell <bobclewell@gmail.com>
2013-02-17 17:22:03 -0500

    Creating sym link to database.yml in deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,6 +22,15 @@ role :app, 'abe'                          # This may be the same as your `Web` s
 role :db,  'abe', :primary => true # This is where Rails migrations will run
 role :db,  'abe'
 
+after 'deploy:update_code', 'deploy:symlink_db'
+
+namespace :deploy do
+  desc "Symlinks the database.yml"
+  task :symlink_db, :roles => :app do
+    run "ln -nfs #{deploy_to}/shared/config/database.yml #{release_path}/config/database.yml"
+  end
+end
+
 # after "deploy:update_code", "bundle:install"
  
 # namespace :bundle do
