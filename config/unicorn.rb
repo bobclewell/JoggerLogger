86e680ef4bd7c01ceb5f0834ddaa6675fcf75ceb
Bob Clewell <bobclewell@gmail.com>
2013-02-17 20:11:42 -0500

    Initial unicorn config.

---
+++ config/unicorn.rb
@@ -0,0 +1,8 @@
+working_directory "/var/www/joggerlogger.net/current"
+pid "/var/www/joggerlogger.net/current/tmp/pids/unicorn.pid"
+stderr_path "/var/www/joggerlogger.net/current/log/unicorn.log"
+stdout_path "/var/www/joggerlogger.net/current/log/unicorn.log"
+
+listen "/tmp/unicorn.joggerlogger.sock"
+worker_processes 2
+timeout 30
